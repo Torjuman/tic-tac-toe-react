bc02fdd2353976a528221cf15bf8106730c8e808
Torjuman <torjumanrimon7@gmail.com>
2023-01-29 14:18:39 +0600

    basic layout made for the game

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
+import { useState } from "react";
 import "./App.css";
 import Icon from "./components/Icons";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { Card, CardBody, Container, Button, Row, Col } from "reactstrap";
+import { FaCentercode } from "react-icons/fa";
+const itemArray = new Array(9).fill("empty");
+
 const App = () => {
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+
+  const reloadGame = () => {
+    setIsCross(false);
+    setWinMessage("");
+    itemArray.fill("empty", 0, 9);
+  };
+
+  const checkIsWinner = () => {
+    //
+  };
+
+  const changeItem = (itemNumber) => {
+    if (winMessage) {
+      return toast(winMessage, { type: "success" });
+    }
+
+    if (itemArray[itemNumber] === "empty") {
+      itemArray[itemNumber] = isCross ? "cross" : "circle";
+      setIsCross(!isCross);
+    } else {
+      return toast("Already filled up", { type: "error" });
+    }
+
+    checkIsWinner();
+  };
+
   return (
-    <div>
-      <Icon />
-    </div>
+    <Container className="p-5">
+      <ToastContainer position="bottom-center" />
+      <Row>
+        <Col md={6} className="offset-md-3">
+          {winMessage ? (
+            <div className="mb-2 mt-2">
+              <h1 className="text-success text-uppercase text-center">
+                {winMessage}
+              </h1>
+              <Button color="success" block onClick={reloadGame}>
+                Reload The Game
+              </Button>
+            </div>
+          ) : (
+            <h1 className="text-center text-warnning">
+              {isCross ? "Cross" : "Circle"} turns
+            </h1>
+          )}
+          <div className="grid">
+            {itemArray.forEach((item, index) => {
+              <Card>
+                <CardBody className="box">
+                  <Icon name={item} />
+                </CardBody>
+              </Card>;
+            })}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
